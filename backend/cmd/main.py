f656c03fc0f3cdaa52ca2a1ec8d92862159e286e
Mover-R <denis.tugow2014@gmail.com>
2025-11-01 23:06:44 +0300

    database check

--- backend/cmd/main.py
+++ backend/cmd/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
+from typing import Optional, Tuple
 import uvicorn
+import asyncio
 import sys
 import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from internal.config.config import Config
+from pkg.postgres.postgres import Database
 
 app = FastAPI()
 
@@ -17,11 +20,30 @@ async def root():
 async def health():
     return {"status": "healthy"}
 
-if __name__ == "__main__":
+async def start():
     cfg = Config()
     print("Config loaded:", cfg)
-    uvicorn.run(
-        app, 
-        host=cfg.rest_host, 
-        port=cfg.rest_port  # Используем порт из конфига
+    
+    psgDB = Database(cfg.postgres)
+    success, err = await psgDB.Connect()
+    if success == False:
+        print(err)
+        return
+    
+    success, err = await psgDB.PingDB()
+    if success == False:
+        print(err)
+        return
+    
+    
+    config = uvicorn.Config(
+        app,
+        host=cfg.rest_host,
+        port=cfg.rest_port,
+        log_level="info"
     )
+    server = uvicorn.Server(config)
+    await server.serve()
+
+if __name__ == "__main__":
+    asyncio.run(start())
--- backend/pkg/postgres/postgres.py
+++ backend/pkg/postgres/postgres.py
@@ -1,5 +1,7 @@
 from pydantic import Field, PostgresDsn, RedisDsn
 from pydantic_settings import BaseSettings
+from typing import Optional, Tuple
+import asyncpg
 
 class PostgresConfig(BaseSettings):
     host: str = Field(..., env="POSTGRES_HOST")
@@ -7,6 +9,8 @@ class PostgresConfig(BaseSettings):
     user: str = Field(..., env="POSTGRES_USER")
     password: str = Field(..., alias="POSTGRES_PASS")
     database: str = Field(..., alias="POSTGRES_DB")
+    min_conn: int = Field(1, alias="POSTGRES_MIN_CONN")
+    max_conn: int = Field(2, alias="POSTGRES_MAX_CONN")
     
     class Config:
         env_file = "./config/.env"
@@ -17,3 +21,57 @@ class PostgresConfig(BaseSettings):
     @property
     def url(self) -> str:
         return f"postgresql://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}"
+    
+ 
+class Database:
+    def __init__(self, cfg: PostgresConfig):
+        self.cfg = cfg
+        self.pool: Optional[asyncpg.Pool] = None
+    async def Connect(self) -> Tuple[bool, str]:
+        try:
+            self.pool = await asyncpg.create_pool(
+                user=self.cfg.user,
+                password=self.cfg.password,
+                database=self.cfg.database,
+                host=self.cfg.host,
+                port=self.cfg.port,
+                min_size=self.cfg.min_conn,  # minimum conn count
+                max_size=self.cfg.max_conn  # maximum conn count
+            )
+            return (True, "")
+        except Exception as e:
+            return (False, f"Connect error: { str(e) }")
+        
+    async def PingDB(self) -> Tuple[bool, str]:
+        """
+        Pings the PostgreSQL database using asyncpg by attempting a simple query.
+        Returns True if the ping is successful, False otherwise.
+        """
+        try:
+            await self.fetch('SELECT 1')
+            await self.pool.close()
+            return (True, "")
+        except asyncpg.exceptions.PostgresError as e:
+            return (False, f"Database connection error: {e}")
+        except Exception as e:
+            return (False, f"An unexpected error occurred: {e}")
+    
+    async def disconnect(self):
+        """Закрытие пула подключений"""
+        if self.pool:
+            await self.pool.close()
+    
+    async def fetch(self, query: str, *args):
+        """Выполнение SELECT запроса"""
+        async with self.pool.acquire() as conn:
+            return await conn.fetch(query, *args)
+    
+    async def fetchrow(self, query: str, *args):
+        """Выполнение SELECT запроса с возвратом одной строки"""
+        async with self.pool.acquire() as conn:
+            return await conn.fetchrow(query, *args)
+    
+    async def execute(self, query: str, *args):
+        """Выполнение INSERT/UPDATE/DELETE запроса"""
+        async with self.pool.acquire() as conn:
+            return await conn.execute(query, *args)
